a12da2a80b9a6d68d5e54acdd398c42ca4dff01f
solyrion <sde101573@gmail.com>
2024-05-05 21:47:35 +0900

    fix: DB수정 및 추가

---
+++ src/main/java/project/capstone/domain/disease/Disease_info.java
@@ -0,0 +1,25 @@
+package project.capstone.domain.disease;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Disease_info {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Lob
+    private String description;
+
+    public Disease_info(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
