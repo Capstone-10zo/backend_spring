a57458f04e334836ee197dfa2366ba402f919e2f
solyrion <sde101573@gmail.com>
2024-05-19 18:14:17 +0900

    feat: 사진 파일 API 설정

--- src/main/java/project/capstone/api/HomeController.java
+++ src/main/java/project/capstone/api/HomeController.java
@@ -1,19 +1,74 @@
 package project.capstone.api;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
+import project.capstone.domain.api.PredictionResponse;
+import project.capstone.service.ChatbotService;
+import project.capstone.service.PetService;
+import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.util.Map;
+
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*") // 모든 도메인에서의 접근을 허용
+//172.30.1.88
 public class HomeController {
+    private static final String FASTAPI_URL = "http://localhost:8000";
+    private final WebClient webClient = WebClient.builder().baseUrl(FASTAPI_URL).build();
+    private final PetService petService;
+    private final ChatbotService chatbotService;
 
-    @CrossOrigin(origins = "*") // 모든 도메인에서의 접근을 허용
     @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/fastapi")
+    public Mono<String> fastapi_date(){
+        Mono<String> mono = webClient.get()
+                .uri("/data")
+                .retrieve()
+                .bodyToMono(String.class);
+        return mono;
+    }
+
+    @GetMapping("/api")
+    public Map<String, PredictionResponse>  api(){
+        Map<String, PredictionResponse> response = webClient.get()
+                .uri("/predict")
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+        return response;
+    }
+
+    // http://172.30.1.88:8080/api2 집 기준
+//    @PostMapping("/api2")
+//    public Mono<String> api2(@RequestParam("file") MultipartFile file) throws IOException {
+//        return petService.ModelResultWithImage(file);
+//    }
+
+    @PostMapping("/api2")
+    public String api2(@RequestParam("file") MultipartFile file) throws IOException {
+        return "ok";
+    }
+
+//    @PostMapping("/chatbot")
+//    public String chatbot(@RequestParam("request") String request){
+//        chatbotService.getResponse(request);
+//        return "ok";
+//    }
 
 }
 
--- src/main/java/project/capstone/service/PetService.java
+++ src/main/java/project/capstone/service/PetService.java
@@ -1,15 +1,32 @@
 package project.capstone.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
+import project.capstone.domain.disease.Disease_info;
 import project.capstone.domain.domain.Member;
 import project.capstone.domain.domain.Pet;
+import project.capstone.exception.NoMatchImageException;
+import project.capstone.repository.disease.DiseaseRepositoryJpa;
 import project.capstone.repository.member.MemberRepository;
 import project.capstone.repository.pet.PetJpaRepository;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.io.IOException;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -17,14 +34,67 @@ public class PetService {
 
     private final PetJpaRepository petJpaRepository;
     private final MemberRepository memberRepository;
+    private final DiseaseRepositoryJpa diseaseRepository;
+    private final JsonProcessingService jsonProcessingService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public Pet join(Pet pet, Long memberId){
+    private final WebClient webClient = WebClient.builder().baseUrl(FASTAPI_URL).build();
+
+    private static final String FASTAPI_URL = "http://localhost:8000";
+
+    public Pet join(Pet pet, Long memberId) {
         Pet savedPet = petJpaRepository.save(pet);
         Member findMember = memberRepository.findById(memberId).get();
         savedPet.changeMember(findMember);
         return savedPet;
     }
 
+    public Mono<String> ModelResultWithImage(MultipartFile file) throws IOException {
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part("file", new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return "file.jpg";  // 파일 이름 설정
+            }
+        }, MediaType.IMAGE_JPEG);  // 올바른 MIME 타입 사용 / JPEG로 통일
 
+        try {
+            Mono<String> result = webClient
+                    .post()
+                    .uri("/predict2")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .bodyValue(multipartBodyBuilder.build())
+                    .retrieve()
+                    .bodyToMono(String.class);
+            Mono<Optional<Disease_info>> description = findDescription(result);
 
+            return Mono.zip(result, description, (s1, s2) -> {
+                try {
+                    ObjectNode jsonNode = (ObjectNode) objectMapper.readTree(s1);
+                    s2.ifPresent(diseaseInfo -> jsonNode.put("description", diseaseInfo.getDescription()));
+                    return jsonNode.toString();
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+
+            });
+
+        } catch (Exception e) {
+            throw new NoMatchImageException(e.getMessage());
+        }
+    }
+
+    public Mono<Optional<Disease_info>> findDescription(Mono<String> result) throws JsonProcessingException {
+
+        return result.flatMap(json -> {
+            try {
+                JsonNode jsonNode = objectMapper.readTree(json);
+                String disease_name = jsonNode.path("disease_name").asText();
+                return Mono.fromCallable(() -> diseaseRepository.findByName(disease_name))
+                        .subscribeOn(Schedulers.boundedElastic());
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 }
